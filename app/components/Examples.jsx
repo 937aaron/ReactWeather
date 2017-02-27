be162bad1fb362ab140ad92b3563c9b40851a5ba
Aaron Newcomer <aaron@automatedinsights.com>
2017-02-26 19:19:57 -0500

    style changes to Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,21 @@
-var React = require('react')
-
+var React = require('react');
+var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the examples page.</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out.</p>
+      <ol>
+        <li>
+          <Link to='/?location=Dayton'>Dayton, OH</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+        <li>
+          <Link to='/?location=Durham'>Durham, NC</Link>
+        </li>
+      </ol>
     </div>
   )
 };
