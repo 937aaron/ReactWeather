0b2bbf26251f8f6d12849e27bcd362395fe6cccc
Aaron Newcomer <aaron@automatedinsights.com>
2017-02-26 19:46:32 -0500

    about styling change

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,11 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcomer to the about page.</p>
-    </div>
+      <h1 className="text-center">About</h1>
+      <h5>Thank you for visiting my little React app!</h5>
+      <p>React weather was built with React(obvs), Foundation, and Webpack. I hope you enjoy your stay.</p>
+        <a target="_blank" className="button" href="https://github.com/937aaron/ReactWeather">Cash me on github >></a>
+  </div>
   )
 }
 
